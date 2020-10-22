98b358d63c476ddd506279cc6388bf2affd01309
shinnosuke-K <shin.050195@gmail.com>
2020-10-22 15:18:53 +0900

    add: パラメータを解釈するハンドラ

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"strconv"
+	"strings"
 
 	meander "github.com/shinnosuke-K/go-meander"
 )
@@ -15,6 +17,17 @@ func main() {
 	meander.APIKey = os.Getenv("GOOGLE_PLACES_API")
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
+	})
+	http.HandleFunc("/recommendations", func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("lat"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lng, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
 	})
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
