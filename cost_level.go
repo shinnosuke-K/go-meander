b7937bf001fbb1d29151ce7d9b83e41dd7740dd1
shinnosuke-K <shin.050195@gmail.com>
2020-10-22 14:56:57 +0900

    add: コストの上限と下限の指定処理

--- cost_level.go
+++ cost_level.go
@@ -1,5 +1,7 @@
 package go_meander
 
+import "strings"
+
 type Cost int8
 
 const (
@@ -26,4 +28,25 @@ func (l Cost) String() string {
 		}
 	}
 	return "不正な値です"
+}
+
+func ParseCost(s string) Cost {
+	return costStrings[s]
+}
+
+type CostRange struct {
+	From Cost
+	To   Cost
+}
+
+func (r CostRange) String() string {
+	return r.From.String() + "..." + r.To.String()
+}
+
+func ParseCostRange(s string) *CostRange {
+	segs := strings.Split(s, "...")
+	return &CostRange{
+		From: ParseCost(segs[0]),
+		To:   ParseCost(segs[1]),
+	}
 }
